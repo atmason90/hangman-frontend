a1040417b19ac719c71c73a11c516acd154f3da3
atmason90 <atmason90@gmail.com>
2023-04-27 13:45:27 -0700

    finished game vs computer basic functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 // import {LoginPage} from './components/LoginPage/LoginPage';
 // import { Leaderboard } from './components/Leaderboard/Leaderboard';
-import { GameComputer } from './components/Game/GameComputer/GameComputer';
+import GameComputer from './components/Game/GameComputer/GameComputer';
 
 // need to add conditional statement to jsx - if not logged in, show login page, otherwise show landing page. (will need useState and useEffect)
 
