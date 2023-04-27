0d7d3aac16d4fbec00e13162482271ec8e639261
atmason90 <atmason90@gmail.com>
2023-04-27 13:35:56 -0700

    add game component for game vs computer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-import {LoginPage} from './components/LoginPage/LoginPage';
-import { Leaderboard } from './components/Leaderboard/Leaderboard';
+// import {LoginPage} from './components/LoginPage/LoginPage';
+// import { Leaderboard } from './components/Leaderboard/Leaderboard';
+import { GameComputer } from './components/Game/GameComputer/GameComputer';
 
+// need to add conditional statement to jsx - if not logged in, show login page, otherwise show landing page. (will need useState and useEffect)
 
 function App() {
   return (
     <div className="App">
-      <LoginPage/>
-      <Leaderboard/>
+      {/* <LoginPage/>
+      <Leaderboard/> */}
+      <GameComputer/>
     </div>
   );
 }
