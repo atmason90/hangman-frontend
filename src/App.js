ec545ca46f1d5ca9bf6ae87935ebf0d8861a1695
hvenka17 <hvenka17@asu.edu>
2023-04-27 23:23:17 -0700

    login routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
-
-// import {LoginPage} from './components/LoginPage/LoginPage';
+import { Routes, Route } from 'react-router-dom';
+import {LoginPage} from './components/LoginPage/LoginPage';
+import { SignupPage } from './components/SignupPage/SignupPage';
 // import { Leaderboard } from './components/Leaderboard/Leaderboard';
 import GameComputer from './components/Game/GameComputer/GameComputer';
+import GamePlayer from './components/Game/GamePlayer/GamePlayer';
 
 // need to add conditional statement to jsx - if not logged in, show login page, otherwise show landing page. (will need useState and useEffect)
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginPage/>
-      <Leaderboard/> */}
-      <GameComputer/>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/compgame' element={<GameComputer/>}/>
+        <Route path='/vsgame' element={<GamePlayer/>}/>
+      </Routes>
     </div>
   );
 }
